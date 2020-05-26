406d058c004547b4168ec9db6fbccd839e6d997a
devansh3008 <30910269+devansh3008@users.noreply.github.com>
2020-05-26 23:05:31 +0530

    Add files via upload

---
+++ sub_domain_enum.py
@@ -0,0 +1,29 @@
+print("------------------------------------SUBDOMAINENUM-----------------------------------------------------------------------------------------------------------------")
+import requests
+import optparse
+parser=optparse.OptionParser()
+parser.add_option("-d","--domain",dest="domain",help="Enter the domain:")
+parser.add_option("-w","--wordlist",dest="wordlist",help="Enter the wordlist")
+options,arguments=parser.parse_args()
+
+def request(url):
+    try:
+        return requests.get("https://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
+    except requests.exceptions.InvalidURL: #this error otherwise crashes it
+        pass
+    except KeyboardInterrupt:
+     print("Exiting the Program")
+     exit()
+
+target_url=str(options.domain)
+target_wordlist=list=str(options.wordlist)
+with open(target_wordlist,'r') as wordfile:
+    for line in wordfile:
+        word = line.strip()
+        test_url = word + '.' + target_url
+        response = request(test_url)
+        if response:
+         if target_wordlist and target_url:
+          print(test_url + '   ' + str(response))
